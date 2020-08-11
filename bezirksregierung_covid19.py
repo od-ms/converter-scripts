6c18c28c937b4465b0b153c6d789d6f7c0edf858
Thomas Werner <opendata@citeq.de>
2020-08-11 14:38:56 +0200

    Fix regex pattern

--- bezirksregierung_covid19.py
+++ bezirksregierung_covid19.py
@@ -57,7 +57,10 @@ print("Latest entry on website:", today)
 
 # Parse COVID-19 numbers
 numPat = r'[^(]*\((-?[\d.]+)\)'
-pattern = r'<li><strong>([SK][^:]+)[^<]*<\/strong>[^<]*[aA]ktuell Infizierte\s*-?([\d.]+)' + numPat+ r'[^<]*Infizierte\s*([\d.]+)'+numPat+r'[^,]*,\s*Verstorbene\s*([\d.]+)'+numPat+r'[^,]*,\s*Genesene\s*([\d.]+)'+numPat
+pattern = r'<li><strong>([SK][^:]+)[^<]*<\/strong>[^/]*[aA]ktuell Infizierte\s*-?([\d.]+)' \
+    + numPat+ r'[^<]*Infizierte\s*([\d.]+)' + numPat \
+    + r'[^,]*,\s*Verstorbene\s*([\d.]+)' + numPat \
+    + r'[^,]*,\s*Genesene\s*([\d.]+)' + numPat
 result = re.findall(pattern, htmlPage.replace('&uuml;', 'ü'))
 print("Parsed data from website:")
 pp = pprint.PrettyPrinter(width=160)
