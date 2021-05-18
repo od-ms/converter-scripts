f00af63dc9f76a066417f5e31cf75feb4b4b42bf
Thomas Werner <opendata@citeq.de>
2021-05-18 22:19:35 +0200

    add config & ignore logs

--- config.dist.py
+++ config.dist.py
@@ -1,4 +1,9 @@
 fahrradverleihe_url = ""
+
+# config for eco-counter-api-reader.py
 eco_counter_token = "token"
 eco_counter_api_url = "url without trailing slash"
 eco_counter_ids = [1,2,3] # Eco counter siteIDs that should be exported
+
+# config für aasee
+datahub_api_url = "https://{}/api/device/{}/packets?auth={}"
