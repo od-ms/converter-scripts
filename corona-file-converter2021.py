dedf4478141a9325724b922bf46d2f2a86c5120d
Thomas Werner <opendata@citeq.de>
2022-06-01 11:33:58 +0200

    Add better debugging information

--- corona-file-converter2021.py
+++ corona-file-converter2021.py
@@ -5,11 +5,13 @@ import logging
 from datetime import datetime, timedelta
 
 # Basic logger configuration
-logging.basicConfig(level=logging.DEBUG, format='<%(asctime)s %(levelname)s> %(message)s')
+logging.basicConfig(level=logging.INFO, format='<%(asctime)s %(levelname)s> %(message)s')
+# Colors on the console
+logging.addLevelName( logging.WARNING, "\033[1;31m%s\033[1;0m" % logging.getLevelName(logging.WARNING))
+logging.addLevelName( logging.ERROR, "\033[1;41m%s\033[1;0m" % logging.getLevelName(logging.ERROR))
 LOGGER = logging.getLogger(__name__)
 TODAY = datetime.now()
 LOGGER.info("=====> START %s <=====", TODAY)
-
 
 def get_encoding_type(file):
     with open(file, 'rb') as f:
@@ -28,13 +30,14 @@ def convert_file(srcfilename):
     try:
         with open(srcfile, 'r', encoding=from_codec) as f, open(trgfile, 'w', encoding='utf-8') as e:
 
-            firstline = True
+            line_nr = 0
             for line in f:
                 # Match data from first column and remove it
-                matches = re.search('Kalenderwoche (\d+)/(\d+).KW', line, flags=re.IGNORECASE)
+                regex = 'Kalenderwoche (\d+)/(\d+).KW'
+                matches = re.search(regex, line, flags=re.IGNORECASE)
                 line = re.sub(r"^[^;]+;", "", line)
-                if firstline:
-                    firstline = False
+                line_nr = line_nr + 1
+                if line_nr == 1:
                     line = '"JAHR";"WOCHE";' + line
                 else:
                     # For some reason one file is broken / add the missing Gänsefüßchen here
@@ -45,6 +48,11 @@ def convert_file(srcfilename):
                             )
                         LOGGER.debug("replaced line %s", line)
                     # Re- insert data from first column into two new columns
+
+                    if not matches:
+                        LOGGER.error("Did not match Regex '%s' in line %s: %s", regex, line_nr, line)
+                        continue;
+
                     line = "{};{};".format(matches.group(1), matches.group(2)) + line
                 e.write(line.replace(';', ','))
 
