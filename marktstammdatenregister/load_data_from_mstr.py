449ee3f1a7107ee109f053ae7a6c0cc99a6b68c4
Thomas Werner <opendata@citeq.de>
2024-09-12 09:12:00 +0200

    Implement pagination

--- marktstammdatenregister/load_data_from_mstr.py
+++ marktstammdatenregister/load_data_from_mstr.py
@@ -5,12 +5,12 @@ import re
 import json
 import random
 import logging
-import requests
+import os
 import os.path
 import pprint
 import time
 import csv
-import os
+import requests
 from pyfiglet import Figlet
 from datetime import datetime, timezone
 
@@ -18,32 +18,35 @@ from datetime import datetime, timezone
 
 # Basic logger configuration
 logging.basicConfig(level=logging.DEBUG, format='<%(asctime)s %(levelname)s> %(message)s')
-logging.addLevelName(logging.WARNING, "\033[1;31m%s\033[1;0m" % logging.getLevelName(logging.WARNING))
-logging.addLevelName(logging.ERROR, "\033[1;41m%s\033[1;0m" % logging.getLevelName(logging.ERROR))
+logging.addLevelName(logging.WARNING, f"\033[1;31m{logging.getLevelName(logging.WARNING)}\033[1;0m")
+logging.addLevelName(logging.ERROR, f"\033[1;41m{logging.getLevelName(logging.ERROR)}\033[1;0m")
 logging.info("=====> START %s <=====", datetime.now())
 
 # Nicer log files with random fonts
-fonts = (
+FONTS = (
     'puffy slant smslant speed standard thick basic bell c_ascii_ charact1 charact2 charact6 chunky clr6x8 colossal '
     'contessa cosmic crawford demo_1__ drpepper fender graceful gothic'
 )
-HEADLINE_FONT = random.choice(fonts.split())
+HEADLINE_FONT = random.choice(FONTS.split())
 logging.debug("(headline font = '%s')", HEADLINE_FONT)
 
+# Link zum Frontend, mit gesetztem Filter:
+# http://www.marktstammdatenregister.de/MaStR/Einheit/Einheiten/OeffentlicheEinheitenuebersicht?filter=Ort~eq~%27M%C3%BCnster%27~and~Betriebs-Status~eq~%2735%2C37%27~and~Energietr%C3%A4ger~eq~%272495%2C2497%27~and~Gemeindeschl%C3%BCssel~eq~%2705515000%27
 
 BASE_URL = 'https://www.marktstammdatenregister.de/MaStR/Einheit/EinheitJson/GetVerkleinerteOeffentlicheEinheitStromerzeugung?'
 
-# Ok, das ist zwei mal die gleiche URL, nur mit Sortierung einmal von vorn und einmal von hinten
-# Weil Limit von denen ist bei 5000 Ergebnisse. Derzeit gibt es ca. 6000 Anlagen.
-# Wenn wir einmal von vorn und einmal von hinten abfragen, bekommen wir alle Anlagen.
-#
-# !!Sollten das mehr als 10.000 werden, dann gibts aber Problem, dann stimmen die Zahlen nicht mehr
-# und das muss umprogrammiert werden!!
-#
-SOURCE_URLS = [
-    BASE_URL + 'sort=EinheitMeldeDatum-asc&page=1&pageSize=10000&group=&filter=Energietr%C3%A4ger~eq~%272495%2C2497%27~and~Betriebs-Status~eq~%2735%2C37%27~and~Gemeindeschl%C3%BCssel~eq~%2705515000%27~and~Ort~eq~%27M%C3%BCnster%27',
-    BASE_URL + 'sort=EinheitMeldeDatum-desc&page=1&pageSize=10000&group=&filter=Energietr%C3%A4ger~eq~%272495%2C2497%27~and~Betriebs-Status~eq~%2735%2C37%27~and~Gemeindeschl%C3%BCssel~eq~%2705515000%27~and~Ort~eq~%27M%C3%BCnster%27'
-]
+SOURCE_URL = (
+    BASE_URL + 'sort=EinheitMeldeDatum-desc'
+    '&pageSize=5000'
+    '&group=&filter=Ort~eq~%27M%C3%BCnster%27~and~'
+    'Betriebs-Status~eq~%2735%2C37%27~and~'
+    'Energietr%C3%A4ger~eq~%272495%2C2497%27~and~'
+    'Gemeindeschl%C3%BCssel~eq~%2705515000%27'
+)
+
+# https://www.marktstammdatenregister.de/MaStR/Einheit/EinheitJson/GetVerkleinerteOeffentlicheEinheitStromerzeugung?sort=EinheitMeldeDatum-desc&page=1&pageSize=10&group=&filter=Ort~eq~%27M%C3%BCnster%27~and~Betriebs-Status~eq~%2735%2C37%27~and~Energietr%C3%A4ger~eq~%272495%2C2497%27~and~Gemeindeschl%C3%BCssel~eq~%2705515000%27
+
+# https://www.marktstammdatenregister.de/MaStR/Einheit/EinheitJson/GetVerkleinerteOeffentlicheEinheitStromerzeugung?sort=EinheitMeldeDatum-desc&page=1&pageSize=10&group=&filter=Betriebs-Status~eq~%2735%2C37%27~and~Energietr%C3%A4ger~eq~%272495%2C2497%27~and~Gemeindeschl%C3%BCssel~eq~%2705515000%27
 
 # SOURCE_URL = 'https://www.marktstammdatenregister.de/MaStR/Einheit/EinheitJson/GetVerkleinerteOeffentlicheEinheitStromerzeugung?sort=&page=1&pageSize=5896&group=&filter=Energietr%C3%A4ger~eq~%272495%27~and~Betriebs-Status~eq~%2735%2C37%27~and~Gemeindeschl%C3%BCssel~eq~%2705515000%27'
 
@@ -106,17 +109,17 @@ def addSum(my_dict, name, value):
         my_dict[name] = value
 
 
-def collect_data_from_url(mstr_url, id_list, start_values, EnergietraegerName):
-    anlagenJson = readUrlWithCache(mstr_url)
-    anlagen = anlagenJson.get('Data')
+def collect_data_from_url(mstr_url, id_list, start_values, energietraeger_name):
+    """ return Energieträger Type Anlagen, added to id_list """
+    anlagen_json = readUrlWithCache(mstr_url)
+    anlagen = anlagen_json.get('Data')
 
-    query_results_count = int(anlagenJson.get('Total'))
+    query_results_count = int(anlagen_json.get('Total'))
     json_results_count = len(anlagen)
+    if json_results_count < 1:
+        return 0
     logging.info("Anlagen gesamt: %s", query_results_count)
     logging.info("Anlagen in result: %s", json_results_count)
-    if (query_results_count / 2) > json_results_count:
-        logging.error("TOO MANY ANLAGEN IN QUERY RESULT! ")
-        raise ValueError("Please update program code, we need more than 2 requests")
 
     # Initialize values
     wanted_collections = {
@@ -139,7 +142,7 @@ def collect_data_from_url(mstr_url, id_list, start_values, EnergietraegerName):
     doppelt_count = 0
     # Add all anlagen values
     for anlage in anlagen:
-        if anlage["EnergietraegerName"] != EnergietraegerName:
+        if anlage["EnergietraegerName"] != energietraeger_name:
             continue
 
         # Skip anlagen that we already have
@@ -162,12 +165,9 @@ def collect_data_from_url(mstr_url, id_list, start_values, EnergietraegerName):
     return {"Summen": wanted_sums, "Werte": wanted_collections}
 
 
-
-
 def write_json_file(data, outfile_name):
     with open(outfile_name, "w") as outfile:
         json.dump(data, outfile, ensure_ascii=True, indent=2, sort_keys=True)
-
 
 
 def write_csv_file(data, HEAD_ROW, outfile_name):
@@ -180,21 +180,28 @@ def write_csv_file(data, HEAD_ROW, outfile_name):
                 outwriter.writerow([file_name] + row)
 
 
-def save(URLS, TYPE):
+def save(URL, TYPE):
+    """ Parse all result pages and write json File """
     custom_fig = Figlet(font=HEADLINE_FONT, width=120)
-    logging.info("\n" + custom_fig.renderText(TYPE))
+    logging.info("\n%s", custom_fig.renderText(TYPE[0:10]))
 
-    logging.info("# \\.")
-    logging.info("####>>> Processing %s Anlagen <<<", TYPE)
-    logging.info("# /°")
+    exclude_ids = {}
+    accumulated_results = None
+    pagenr = 1
+    while True:
+        logging.info("# \\.")
+        logging.info("####>>> Processing %s Anlagen - Page %s <<<", TYPE, pagenr)
+        logging.info("# /°")
+        page_url = URL + '&page=' + str(pagenr)
+        response = collect_data_from_url(page_url, exclude_ids, accumulated_results, TYPE)
+        if not response:
+            logging.info("EMPTY PAGE - %s is done.", TYPE)
+            break
+        accumulated_results = response
+        pagenr = pagenr + 1
+    write_json_file(accumulated_results, f"anlagen_{TYPE.lower().replace(' ', '_')}.json")
+    logging.info(pprint.pformat(accumulated_results))
 
-    EXCLUDE_IDs = {}
-    RESULT_DATA = None
-    for URL in URLS:
-        RESULT_DATA = collect_data_from_url(URL, EXCLUDE_IDs, RESULT_DATA, TYPE)
-    write_json_file(RESULT_DATA, "anlagen_{}.json".format(TYPE.lower().replace(" ", "_")))
-    logging.info(pprint.pformat(RESULT_DATA))
 
-
-save(SOURCE_URLS, "Wind")
-save(SOURCE_URLS, "Solare Strahlungsenergie")
+save(SOURCE_URL, "Wind")
+save(SOURCE_URL, "Solare Strahlungsenergie")
