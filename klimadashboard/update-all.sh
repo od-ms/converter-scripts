aa5d82442c1a2e8ddf455f48c3bb46d60ffcfb8d
Thomas Werner <opendata@citeq.de>
2026-02-20 15:32:57 +0100

    Fix update script

--- klimadashboard/update-all.sh
+++ klimadashboard/update-all.sh
@@ -2,16 +2,22 @@
 
 set -e
 
-echo "\n########### Updating MARKTSTAMMDATENREGISTER...\n"
+headline() {
+  RED='\033[1;93m\033[5m\033[45m'
+  NC='\033[0m' 
+  echo -e "\n${RED}########### $1 ###########${NC}\n"
+}
+
+headline "Updating MARKTSTAMMDATENREGISTER..."
 pushd ../marktstammdatenregister
 ./generate_and_copy_files.sh
 popd
 
-echo "\n########### Generate PV Anlagen files...\n"
+headline "Generate PV Anlagen files..."
 python3 generate_pv_anlagen.py
 
-echo "\n########### Generate Klimadashboard Datafile...\n"
+headline "Generate Klimadashboard Datafile..."
 python3 ./split_datafile.py
 
-echo "\n########### DONE\n"
+headline "DONE"
 
